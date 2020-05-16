06a073477bddb290b07d4a3bfcff1be34846d785
Venkata-g1 <65384071+Venkata-g1@users.noreply.github.com>
2020-05-16 18:44:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,11 +14,13 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [float(x) for x in request.form.values()]
+    int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 10)
+    if(output >= .80):
+        return ("please contact a doctor")
 
     return render_template('index.html', prediction_text='Affecting with heart disease rate : {}'.format(output))
 
